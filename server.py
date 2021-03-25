50f9338ea21f585914c839d81699d8fc76d5d253
Shubhranshu-Malhotra <shubhranshumalhotra.sm@gmail.com>
2021-03-25 15:48:01 +0530

    Making a basic server using flask

--- server.py
+++ server.py
@@ -1,9 +1,21 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 print(__name__)
 
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!!'
+    return render_template('index.html') #'Hello World!!'
+
+@app.route('/about')
+def about():
+    return render_template('about.html') #'Hello World!!'
+
+@app.route('/blog')
+def blog_world():
+    return 'Welcome to the blog world!!'
+
+@app.route('/blog/2020/dogs')
+def dogs_blog():
+    return 'This is a blog on dogs!!'
 
